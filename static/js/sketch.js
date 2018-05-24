1746d55f534f6f43f2bbafd1b40dc535974a8a6d
Madison Burke <madisononealburke@gmail.com>
2018-05-24 05:37:04 -0700

    socket front end test

--- static/js/sketch.js
+++ static/js/sketch.js
@@ -1,8 +1,13 @@
+images = ['standup', 'walkup1', 'walkup2'
+		  'standdown', 'walkdown1'
+		  'walkdown2']
+frame = 0;
+
 function setup(){
 	createCanvas(100, 50)
 	background(153)
 }
 
 function draw(){
-
+	image(images[frame%6], 0, 0);
 }
