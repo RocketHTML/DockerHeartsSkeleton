3bd6b86cf55afe2710f95f0347eb62b50c893f8b
Madison Burke <madisononealburke@gmail.com>
2018-05-24 05:45:30 -0700

    socket front end test

--- static/js/sketch.js
+++ static/js/sketch.js
@@ -1,8 +1,15 @@
-images = ['standup', 'walkup1', 'walkup2',
+poses = ['standup', 'walkup1', 'walkup2',
 		  'standdown', 'walkdown1',
 		  'walkdown2']
+imgs = []
 dir = 'static/ayanaposes/'
 frame = 0;
+
+function preload(){
+	for (let i = 0; i < poses.length; i++){
+		imgs[i] = loadImage(dir + poses[i])
+	}
+}
 
 function setup(){
 	createCanvas(100, 50)
@@ -10,5 +17,5 @@ function setup(){
 }
 
 function draw(){
-	image(dir + images[frame%6], 0, 0)
+	image(imgs[frame%imgs.length], 0, 0)
 }
