71ba622b0b4c0b053ad97e0ccfd2c786af83d9af
Madison Burke <madisononealburke@gmail.com>
2018-05-24 03:55:26 -0700

    socket front end test

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
@@ -9,6 +9,10 @@ socketio = SocketIO(app)
 def handleMessage(msg):
 	print('Message: ' + msg)
 	send(msg, broadcast=True)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
 
 if __name__ == '__main__':
 	socketio.run(app, host='0.0.0.0', port=80, debug=True)
